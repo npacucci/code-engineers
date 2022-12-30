fe4a44ec2a518945f77f86b420dc707a01a5b382
Nicola <nicola.pacucci@burda-forward.it>
2022-12-30 15:05:12 +0100

    Setup tailwind, clenup demo code

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'black': '#000',
+			'white': '#fff',
+			'blue': '#90a8ed',
+			'pink': '#ff90e8',
+			'orange': '#ffb7a3',
+			'green': '#23a094',
+			'yellow': '#ffc900',
+			'gray': '#90aab7'
+		},
+		fontFamily: {
+			sans: ['Inter', 'system-ui', 'sans-serif'],
+			// serif: ['Merriweather', 'serif'],
+			// sans: ['Graphik', 'sans-serif'],
+			// serif: ['Merriweather', 'serif'],
+		},
+		container: {
+			center: true,
+		},
+		extend: {},
+	},
+	plugins: [],
+}
